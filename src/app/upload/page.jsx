88cabb41ea6d4ffbdc7d9105a2ac4bcf56b67df3
Amin-Asadullayev <aminvit693@gmail.com>
2025-09-13 17:49:24 +0400

    okay

--- src/app/upload/page.jsx
+++ src/app/upload/page.jsx
@@ -1,5 +1,10 @@
+"use client"
 import Navbar from "@/components/Navbar"
-import Canva from "@/components/Canva"
+import dynamic from 'next/dynamic';
+
+const Canva = dynamic(() => import('@/components/Canva'), {
+  ssr: false, 
+});
 
 export default function App(){
     return (
